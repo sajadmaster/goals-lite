f9972bb711ac0cbf25b7cb3230744ff701944e01
sim <sajad@simsysglobal.com>
2022-01-07 10:50:21 +0900

    login Page: skeleton finished

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -10,6 +10,31 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        // background image
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/login_bg.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        // Bottom Login Container
+        Positioned.fill(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
   }
 }
