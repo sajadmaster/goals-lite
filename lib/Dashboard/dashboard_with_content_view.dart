b031036d5611027a7a548cb8dfa797f95e329058
sim <sajad@simsysglobal.com>
2022-01-09 15:06:06 +0900

    goal card view progressing

--- lib/Dashboard/dashboard_with_content_view.dart
+++ lib/Dashboard/dashboard_with_content_view.dart
@@ -13,7 +13,7 @@ class _DashboardWithContentState extends State<DashboardWithContent> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -21,14 +21,14 @@ class _DashboardWithContentState extends State<DashboardWithContent> {
           Text(
             'Goals',
             style: TextStyle(
-                fontSize: 14, fontWeight: FontWeight.bold, color: black2),
+                fontSize: 18, fontWeight: FontWeight.bold, color: black2),
           ),
           SizedBox(height: 9),
           Expanded(
             child: ListView(
-              padding: const EdgeInsets.all(8),
               children: <Widget>[
                 GoalCard(),
+                SizedBox(height: 10),
                 GoalCard(),
               ],
             ),
--- lib/Dashboard/goal_card_view.dart
+++ lib/Dashboard/goal_card_view.dart
@@ -1,36 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:goals_lite/_shared/my_colors.dart';
 
 class GoalCard extends StatelessWidget {
   const GoalCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Container(
+      height: 160,
       child: Card(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const ListTile(
-              title: Text('Run'),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 10, top: 10),
+              child: Row(
+                children: [
+                  Text(
+                    'Run',
+                    style: TextStyle(
+                        color: black2,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () => {},
+                    icon: Icon(Icons.more_vert),
+                  )
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: Row(
+                children: [
+                  RecordColumn(),
+                  Spacer(),
+                  RecordColumn(),
+                  Spacer(),
+                  RecordColumn(),
+                ],
+              ),
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class RecordColumn extends StatelessWidget {
+  RecordColumn();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          '8.5',
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.bold, color: black2),
+        ),
+        SizedBox(height: 2),
+        Text('Today (km)'),
+      ],
     );
   }
 }
