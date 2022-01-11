25d2bd7f25beaa09285f58deb885f28139c19c21
sim <sajad@simsysglobal.com>
2022-01-11 18:37:41 +0900

    dashboard adjust goal unit new design

--- lib/Dashboard/record_column_widget.dart
+++ lib/Dashboard/record_column_widget.dart
@@ -10,12 +10,22 @@ class RecordColumn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          '8.5',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: black2),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.alphabetic,
+          children: [
+            Text(
+              '8.5',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: black2),
+            ),
+            Text(
+              goal.getUnit,
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: black2),
+            ),
+          ],
         ),
         SizedBox(height: 2),
-        Text('Today (${goal.getUnit})'),
+        Text('Today'),
       ],
     );
   }
